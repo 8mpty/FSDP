5f23b988ee3d004e88c88fd68b73608bc085c139
8mpty <mohdkhairin62@gmail.com>
2023-07-28 00:32:01 +0800

    added basic vite client stuff

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link }
+  from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <AppBar position="static" className='AppBar'>
+        <Container>
+          <Toolbar disableGutters={true}>
+            <Link to="/">
+              <Typography variant="h6" component="div">
+                Learning
+              </Typography>
+            </Link>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container>
+        <Routes>
+          <Route path={"/"} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
+export default App;
