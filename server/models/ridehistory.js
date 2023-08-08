d271575754f8f8aae4489dab41fd9d51bb547081
gabrielquek5 <gabrielquek5@gmail.com>
2023-08-08 18:15:00 +0800

    updated ridehistory model and page to use booking

--- server/models/ridehistory.js
+++ server/models/ridehistory.js
@@ -1,34 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
   const ridehistory = sequelize.define("ridehistory", {
-    driver: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    rider: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    bookingId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     description: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
-    start: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    end: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
+
     points: {
       type: DataTypes.INTEGER,
       allowNull: false,
 
     },
-    role: {
-      type: DataTypes.ENUM("driver", "rider"),
-      allowNull: false
-    }
+    totalPoints: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+
+    },
+
   });
   ridehistory.associate = (models) => {
     ridehistory.belongsTo(models.User, {
@@ -36,6 +27,21 @@ module.exports = (sequelize, DataTypes) => {
       as: 'user'
     });
   };
+
+  ridehistory.associate = (models) => {
+    ridehistory.belongsTo(models.Booking, {
+      foreignKey: "bookingId",
+      as: 'booking'
+    });
+  };
+
+  ridehistory.associate = (models) => {
+    ridehistory.belongsTo(models.DriverBooking, {
+      foreignKey: "driverbookingId",
+      as: 'driverbooking'
+    });
+  };
+
   return ridehistory;
 
 };
